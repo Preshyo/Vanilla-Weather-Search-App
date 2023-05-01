66c03981c9062f1dfc3f70d8918da21b9779481a
Preshyo <66226812+Preshyo@users.noreply.github.com>
2023-05-01 11:40:33 +0100

    Added the forecast HTML , CSS and JS template

--- src/app.js
+++ src/app.js
@@ -10,6 +10,22 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    let forecastHTML = `<div class = "row">`;
+    days.forEach(function (day) {
+        forecastHTML = forecastHTML + `<div class="col-2">
+        <div class="weather-forecast-date"> ${day} </div>
+        <img src="http://openweathermap.org/img/wn/50d@2x.png" alt=" " width= "42"/>
+        <div class = "weather-forecast-temperature">
+        <span class = "weather-forecast-temperature-max"> 18° </span>
+        <span class = "weather-forecast-temperature-min"> 12° </span> </div> </div>`;
+    });
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+
+}
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -80,3 +96,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Nigeria");
+displayForecast();
